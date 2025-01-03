0ace62ac3d9be964d90df68513086280e960f4dc
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-01-03 09:10:17 -0300

    Update Estudiante repositorio

--- src/main/java/juan/estudiantes/repositorio/EstudianteRepositorio.java
+++ src/main/java/juan/estudiantes/repositorio/EstudianteRepositorio.java
@@ -1,8 +1,19 @@
+// Paquete donde se encuentra la interfaz EstudianteRepositorio
 package juan.estudiantes.repositorio;
 
+// Importamos la interfaz JpaRepository de Spring Data JPA
 import org.springframework.data.jpa.repository.JpaRepository;
+
+// Importamos la clase Estudiante del modelo
 import juan.estudiantes.modelo.Estudiante;
 
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer>{
+// Interfaz que define el repositorio para la entidad Estudiante
+// Extiende de JpaRepository para heredar sus métodos CRUD
+public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
+
+    // No es necesario agregar métodos aquí, ya que JpaRepository proporciona métodos CRUD básicos
+    // Sin embargo, se pueden agregar métodos personalizados para consultas específicas
+    // Por ejemplo: @Query("SELECT e FROM Estudiante e")
+    // List<Estudiante> listarEstudiantes();
 
 }
