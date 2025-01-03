16bcf71c46212e4d4649ab1b524957a7d32b6a2e
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-01-03 09:07:22 -0300

    Update Estudiante model

--- src/main/java/juan/estudiantes/modelo/Estudiante.java
+++ src/main/java/juan/estudiantes/modelo/Estudiante.java
@@ -1,32 +1,44 @@
+// Paquete donde se encuentra la clase Estudiante
 package juan.estudiantes.modelo;
 
+// Importamos las anotaciones necesarias para la configuración de la entidad
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+// Importamos las anotaciones de Lombok para reducir el boilerplate
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+// Anotación que indica que esta clase es una entidad de la base de datos
 @Entity
-// boilerplate - codigo repetitivo
-// codigo get & set agregados:
+
+// Anotación que genera el código getter y setter para los atributos de la clase
 @Data 
-// Constructor vacio:
+
+// Anotación que genera un constructor vacío para la clase
 @NoArgsConstructor
-// Agregamos el constructor con todos los argumentos: 
+
+// Anotación que genera un constructor con todos los atributos de la clase
 @AllArgsConstructor
+
+// Anotación que genera el método toString() para la clase
 @ToString
 
 public class Estudiante {
 
-    // Para declarar que es la llave primaria
+    // Atributo que representa la llave primaria de la tabla Estudiante
+    // Anotación que indica que este atributo es la llave primaria
     @Id 
-    // Para asignarle valor a la llave primaria de forma automatica
+    
+    // Anotación que asigna un valor automático a la llave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEstudiante;
 
+    // Atributos que representan los campos de la tabla Estudiante
     private String nombre;
     private String apellido;
     private String telefono;
